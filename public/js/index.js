287bc548bcc4aeee0d128adbf1019be21ba5ca52
Catalina Tarelli <catatarelli@hotmail.com>
2022-10-09 13:34:38 +0200

    Add index.js file and import functions modules

---
+++ public/js/index.js
@@ -0,0 +1,5 @@
+import createWorld from "./createWorld/createWorld.js";
+import goToNextGeneration from "./goToNextGeneration/goToNextGeneration.js";
+import playGameOfLife from "./playGameOfLife/playGameOfLife.js";
+
+playGameOfLife();
