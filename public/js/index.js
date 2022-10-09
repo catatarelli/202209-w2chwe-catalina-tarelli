0f7e2cdca8e3357519137e20aa561d006ab07398
Catalina Tarelli <catatarelli@hotmail.com>
2022-10-09 13:36:37 +0200

    Fix index.js error

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import createWorld from "./createWorld/createWorld.js";
 import goToNextGeneration from "./goToNextGeneration/goToNextGeneration.js";
 import playGameOfLife from "./playGameOfLife/playGameOfLife.js";
