c736cbb985fe254ff018056777fdfc2d928b914d
Catalina Tarelli <catatarelli@hotmail.com>
2022-10-09 13:44:02 +0200

    Fix imports in index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-unused-vars */
-import createWorld from "./createWorld/createWorld.js";
-import goToNextGeneration from "./goToNextGeneration/goToNextGeneration.js";
 import playGameOfLife from "./playGameOfLife/playGameOfLife.js";
 
 playGameOfLife();
