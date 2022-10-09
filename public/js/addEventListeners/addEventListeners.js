616401e3464648510d23220683762261d42cc643
Catalina Tarelli <catatarelli@hotmail.com>
2022-10-09 21:33:57 +0200

    Erase unused querySelectors from addEventListeners

--- public/js/addEventListeners/addEventListeners.js
+++ public/js/addEventListeners/addEventListeners.js
@@ -1,7 +1,5 @@
 import createGrid from "../createGrid/createGrid.js";
 
-const clearButton = document.querySelectorAll(".clear-button");
-const forwardButton = document.querySelector("forward-button");
 const startButton = document.querySelector(".start-button");
 
 const addEventListeners = () => {
